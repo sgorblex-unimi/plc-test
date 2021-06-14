d4ac04d5cd6307fa3b2b9862e8f282436e411a68
Alessandro Clerici <ale.clericil@gmail.com>
2021-06-14 21:37:45 +0200

    fixed boolean var generation

--- GenMML.fsx
+++ GenMML.fsx
@@ -36,31 +36,41 @@ let addenv (addtyp: typ) (name: string) (env: genenv) : genenv =
 let rec genZeroB env =
     match Map.tryFind TypB env with
     | None -> genCstB
-    | Some l -> if List.length l = 0 then failwith "invalid genenv" else Gen.oneof [genCstB; Gen.map Var (Gen.elements l)]
+    | Some l -> if List.length l = 0 then failwith "invalid genenv" else Gen.frequency [(1, genCstB); (2, Gen.map Var (Gen.elements l))]
 
-let rec genB (env: genenv) (s: int) : Gen<tyexpr> =
-    let genPrimB (env: genenv) (s: int) : Gen<tyexpr> =
-        let genAnd (env: genenv) (s: int) : Gen<tyexpr> =
+let rec genI (env: genenv) (n: int) (s: int) : Gen<tyexpr> = gen { return CstI 42 }
+
+let rec genB (env: genenv) (n: int) (s: int) : Gen<tyexpr> =
+    let genPrimB (env: genenv) (n: int) (s: int) : Gen<tyexpr> =
+        let genAnd (env: genenv) (n: int) (s: int) : Gen<tyexpr> =
             let ns = s - 1
             gen { let! s1 = Gen.choose (0, ns)
                   let s2 = ns - s1
-                  let! e1 = genB env s1
-                  let! e2 = genB env s2
+                  let! e1 = genB env n s1
+                  let! e2 = genB env n s2
                   return Prim ("&", e1, e2) }
-        Gen.oneof [genAnd env s]
-        // Gen.oneof [genEquals; genLess; genAnd]
-//    let genLetVar (env: genenv) (s: int) : Gen<tyexpr> =
-//        gen { let! varType = Gen.elements [TypI; TypB]
-//              let newEnv = addEnv env
-//            }
+        Gen.oneof [genAnd env n s]
+//      Gen.oneof [genEquals; genLess; genAnd]
+    let genLetVar (env: genenv) (n: int) (s: int) : Gen<tyexpr> =
+        // gen { let! varType = Gen.elements [TypI; TypB]
+        gen { let! varType = Gen.elements [TypB]
+              let ns = s-1
+              let! s1 = Gen.choose (0, ns)
+              let s2 = ns - s1
+              let nn = n+1
+              let varName = "v" + string nn
+              let newEnv = addenv varType varName env
+              let! e1 = if varType = TypB then genB env nn s1 else genI env nn s1
+              let! e2 = genB newEnv nn s2
+              return Let (varName, e1, e2) }
     match s with
     | 0 -> genZeroB env
-    // | n when n>0 -> Gen.frequency [(3, genPrimB env s); (1, genLetVar env s)]
-    | n when n>0 -> Gen.frequency [(3, genPrimB env s)]
+    // | n when n>0 -> Gen.frequency [(3, genPrimB env n s); (1, genLetVar env n s)]
+    | m when m>0 -> Gen.frequency [(1, genPrimB env n s); (1, genLetVar env n s)]
     | _ -> failwith "invalid size"
 
 let gen =
-    Gen.sized (genB emptyGenenv)
+    Gen.sized (genB emptyGenenv 0)
 
 // let sample1 = Gen.sample 1 1
 let go() = gen |> Gen.sample 0 1
@@ -68,7 +78,7 @@ let gon n = gen |> Gen.sample n 1
 // let bgo = Gen.sized (genB emptyGenenv) |> Gen.sample 0 10
 
 
-let goWithEnv n (e: genenv) = Gen.sized (genB e) |> Gen.sample n 1
+let goWithEnv n (e: genenv) = Gen.sized (genB e 0) |> Gen.sample n 1
 let varTestEnv = emptyGenenv |> Map.add TypB ["x"; "y"]
 let goTE n = goWithEnv n varTestEnv
 
